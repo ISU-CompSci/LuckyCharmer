a7ab894e5905065185183aa6cd94dbc153549092
Jon Holmes <holmjona@isu.edu>
2018-11-06 10:51:02 -0700

    Initial Import of learning about serializability

--- LuckyCharmer/MainWindow.xaml.cs
+++ LuckyCharmer/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,6 +23,44 @@ namespace LuckyCharmer {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void btnReadFile_Click(object sender, RoutedEventArgs e) {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Stuff to read|*.txt;*.fluffy|Fluffy Thingies|*.fluffy";
+            ofd.ShowDialog();
+            //string bob = ofd.FileName;
+            //MessageBox.Show(bob);
+            try {
+                String[] lines = File.ReadAllLines(ofd.FileName);
+                MessageBox.Show(lines[0]);
+            }catch(Exception ex) {
+
+            } finally {
+
+            }
+        }
+
+        private void btnSaveHero_Click(object sender, RoutedEventArgs e) {
+            SaveFileDialog ofd = new SaveFileDialog();
+            FileStream fs = null;
+            try {
+                MyClasses.SuperHero sup = new MyClasses.SuperHero();
+                sup.FirstName = "Bat";
+                sup.LastName = "Man";
+
+                ofd.ShowDialog();
+
+                fs = new FileStream(ofd.FileName, FileMode.OpenOrCreate);
+
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, sup);
+
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            } finally {
+              if (fs != null) fs.Close();
+            }
         }
     }
 }
